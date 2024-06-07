c702d847263ebfe0a7a8af17583b1af89c51fa9d
kr1zzo <enio.krizman@fer.hr>
2024-06-07 18:51:54 +0200

    requrements updated 2

--- path_planning_ws/build/user_action_interfaces/ament_cmake_python/user_action_interfaces/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='user_action_interfaces',
-    version='0.0.0',
-    packages=find_packages(
-        include=('user_action_interfaces', 'user_action_interfaces.*')),
-)
--- path_planning_ws/install/user_action_interfaces/lib/python3.10/site-packages/user_action_interfaces/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from user_action_interfaces.msg._coast_msg import CoastMsg  # noqa: F401
-from user_action_interfaces.msg._path_msg import PathMsg  # noqa: F401
-from user_action_interfaces.msg._start_goal_msg import StartGoalMsg  # noqa: F401
